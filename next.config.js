ee54ae5534c74146a767bef11f467790df47a89b
Satish Venkatakrishnan <satish860@gmail.com>
2023-03-07 11:03:52 +0530

    Initial commit with UI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/server/:path*",
+        destination: "http://web-sht-358a2a24.local.on-acorn.io/:path*",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { themeconst } from "../Chakra/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={themeconst}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
